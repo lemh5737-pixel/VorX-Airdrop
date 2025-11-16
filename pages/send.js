e56c675205ca345838b9f4656e600b05e5ab75bd
lemh5737-pixel <lemh5737@gmail.com>
2025-11-16 18:53:10 +0700

    Update send.js

--- pages/send.js
+++ pages/send.js
@@ -2,7 +2,6 @@ import { useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
 import { database, ref, set, onValue } from '../lib/firebase';
 import { generateDeviceId } from '../utils/generateId';
-import '../styles/global.css';
 
 export default function Send() {
   const [deviceId, setDeviceId] = useState('');
